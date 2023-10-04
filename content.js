6a66cdd6ae637ce2f83e44d04cd4504751017c83
hs4449889 <shida.haruhi@gmail.com>
2023-10-05 08:44:39 +0900

    [upd]ページ作成時に時刻情報も追加するように変更

--- content.js
+++ content.js
@@ -6,6 +6,7 @@
 
             const project = items.selectedProjectName;
             const tags = items.selectedTags ? "#" + items.selectedTags.replace(/\s/g, "").split(",").join(" #") : false;
+            const timestamp = `#${getTimestamp()}`;
 
             if (!project) {
                 alert('はじめに、プロジェクトのURLをセットしましょう!');
@@ -17,8 +18,20 @@
             if (tags) {
                 lines.push("", tags);
             }
+            lines.push(timestamp);
 
             window.open(`https://scrapbox.io/${project}/${encodeURIComponent(title)}?body=${encodeURIComponent(lines.join('\n'))}`);
         });
     });
+
+    function getTimestamp() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    }
 })();
